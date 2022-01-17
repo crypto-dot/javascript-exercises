b732fdd25cab0c1a3171e621713377841dbaf6c0
Carlos Arbizu <carlos2345532@gmail.com>
2022-01-17 00:17:45 -0600

    completed 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    result = 0;
+    for(let i = 0; i < array.length; i++){
+      result += array[i];
+    }
+    return result;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    result = array[0];
+    for(let i = 1; i < array.length; i++){
+      result *= array[i];
+    }
 
+    return result;
+};  
+
+const power = function(num, power) {
+    result = 1;
+    for(let i = 0; i < power;i++){
+      result *= num;
+    }
+    return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0){
+    return 1;
+  }
+  else {
+    num = num * factorial(num-1);
+    return num;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,37 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/ /g,'');
+    string = toAlphanumeric(string);
+    string = string.toLowerCase();\
+    
+    console.log(string);
+    stak = [...string];
+    queue = string.split('');
+
+    while(stak.length !== 0){
+        char = stak[stak.length - 1];
+        char2 = queue[0];
+        if(char != char2){
+            return false;
+        }
+        stak.pop();
+        queue.shift();
+    }
+    return true;
 
 };
+
+const toAlphanumeric =  function (string){
+    let i, len;
+    for(i = 0, len = string.length; i < len; i++){
+        let code = string.charCodeAt(i);
+        if( !(code > 47 && code < 58) &&
+            !(code > 64 && code < 91) && 
+            !(code > 96 && code < 123)){
+                string = string.slice(0,i) + string.slice(i + 1);
+            }
+    }
+    return string;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
