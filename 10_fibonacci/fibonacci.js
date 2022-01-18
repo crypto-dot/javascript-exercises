13a22e8ef2d39ddf5627847959ec679152f2ac9d
Carlos Arbizu <carlos2345532@gmail.com>
2022-01-17 21:40:01 -0600

    completed fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num <= 0){
+        return "OOPS";
+    }
+    let result = 1;
+    let prevResult = 0;
 
+    for(let i = 1; i < num; i++){
+        let temp  = result;
+        result += prevResult;
+        prevResult = temp;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
