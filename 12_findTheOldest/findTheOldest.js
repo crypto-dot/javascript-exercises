f39a2db6085bc3f0a8d6199828742d14f53701ed
Carlos Arbizu <carlos2345532@gmail.com>
2022-01-23 00:52:25 -0600

    finished oldestPerson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    let oldPerson = {name: "Nan", age: 0};
+    persons.forEach(person => {
+        let personAge;
+        if(person["yearOfDeath"] == undefined){
+            personAge = (new Date().getFullYear()) - person["yearOfBirth"];
+        }
+        else {
+            personAge = person["yearOfDeath"] - person["yearOfBirth"];
+        }
+        if(oldPerson["age"] < personAge){
+            oldPerson["name"] = person["name"];
+            oldPerson["age"] = personAge;
+        }
+    });
+    return oldPerson;
 };
 
 // Do not edit below this line
